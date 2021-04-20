5c3cde7edd0a8caa446cd4888c8d51fed275c4f2
Cristian Morales <cr15moresp@gmail.com>
2021-04-20 18:02:57 -0500

    Se aplicaron promesas en cadena para resumir el codigo en el commit anterior

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-fetch( jokeUrl ).then( respuesta => {
-    console.warn('Respuesta');
-    console.log(respuesta);     // Almacena información sobre la peticion hecha
+// fetch( jokeUrl ).then( respuesta => {
+//     console.warn('Respuesta');
+//     console.log(respuesta);     // Almacena información sobre la peticion hecha
 
-    respuesta.json().then( data => {        // En lugar de la data se podria aplicar desestructiracion ({ id, value }) 
-        console.warn('Body de la respuesta');
-        console.log(data);
+//     respuesta.json().then( data => {        // En lugar de la data se podria aplicar desestructiracion ({ id, value }) 
+//         console.warn('Body de la respuesta');
+//         console.log(data);
 
-        console.warn('Valores que necesito');
-        console.log(data.id);
-        console.log(data.value);
-    });
-});
+//         console.warn('Valores que necesito');
+//         console.log(data.id);
+//         console.log(data.value);
+//     });
+// });
+
+fetch( jokeUrl )
+    .then( resp => resp.json() )
+    .then( ({ id, value }) => console.log( id, value ) );    // Estas lineas son exactactamente los mismo al codigo superior comentado
 
