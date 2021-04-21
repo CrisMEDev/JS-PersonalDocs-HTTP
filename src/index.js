8fc9e1ef65ea419c255b325a8d927c3b66cd1bf4
Cristian Morales <cr15moresp@gmail.com>
2021-04-21 13:05:27 -0500

    Se obtuvo una lista de usuarios a partir de una peticion http

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { init } from './js/jokes-page';
+// import { init } from './js/jokes-page';
 
+import { obtenerUsuarios } from "./js/http-provider";
 
+obtenerUsuarios().then( console.log );
 
-init();
+// init();
 
 
--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -1,5 +1,6 @@
 
-const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+const jokeUrl     = 'https://api.chucknorris.io/jokes/random';
+const urlUsuarios = 'https://reqres.in/api/users?page=2';
 
 // fetch( jokeUrl ).then( respuesta => {
 //     console.warn('Respuesta');
@@ -38,7 +39,17 @@ const obtenerChiste = async() => {
 
 }
 
+const obtenerUsuarios = async() => {
+
+    const resp = await fetch(urlUsuarios);
+    const {data:usuarios} = await resp.json();
+    
+    return usuarios;
+
+}
+
 export {
-    obtenerChiste
+    obtenerChiste,
+    obtenerUsuarios
 }
 
