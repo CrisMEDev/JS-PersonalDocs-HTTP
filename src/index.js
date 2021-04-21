c3fdcf493686ac66945eb0ebc1f2c2d94c2c57e8
Cristian Morales <cr15moresp@gmail.com>
2021-04-21 14:13:09 -0500

    Se lee un usuario de una petición http

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // import { init } from './js/jokes-page';
+// import { init } from "./js/usuarios-page";
+import * as CRUD from "./js/crud-provider";     // Toma todo lo que está exportado y asignale el alias CRUD
 
-import { init } from "./js/usuarios-page";
+// init();
 
-
-
-init();
+CRUD.getUsuario( 1 ).then( console.log );       // Lee un usuario de una peticion http
 
 
