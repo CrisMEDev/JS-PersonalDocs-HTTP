e589c8cd7304c1275e65d3bda7b03cf3d74be373
Cristian Morales <cr15moresp@gmail.com>
2021-04-21 22:03:52 -0500

    Se creó el html para la subida de archivos

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 // import { init } from './js/jokes-page';
 // import { init } from "./js/usuarios-page";
+import { init } from "./js/archivos-page";
 import * as CRUD from "./js/crud-provider";     // Toma todo lo que está exportado y asignale el alias CRUD
 
 // init();
 
-CRUD.getUsuario( 1 ).then( console.log );       // Lee un usuario de una peticion http
+// CRUD.getUsuario( 1 ).then( console.log );       // Lee un usuario de una peticion http
 
-CRUD.crearUsuario({
-    name: 'Cristian',
-    job:  'Programmer'
-}).then( console.log );
+// CRUD.crearUsuario({
+//     name: 'Cristian',
+//     job:  'Programmer'
+// }).then( console.log );
 
-CRUD.updateUsuario( 1, {
-    name: 'Aurora',
-    job:  'Production leader'
-}).then( console.log );
+// CRUD.updateUsuario( 1, {
+//     name: 'Aurora',
+//     job:  'Production leader'
+// }).then( console.log );
 
-CRUD.borrarUsuario(2).then( console.log );
+// CRUD.borrarUsuario(2).then( console.log );
+
+init();
 
 
